90d517179cf8af8c5bf7459c259797aa0b9099d7
SreekanthTeja <srikanthteja24@gmail.com>
2021-08-28 21:25:54 +0530

    corsheaders

--- bigspace/settings.py
+++ bigspace/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'bigspace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
