890199b087be105629979dbabb01fd223c2b2506
SreekanthTeja <srikanthteja24@gmail.com>
2021-09-02 16:32:58 +0530

    rechanged

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -18,17 +18,11 @@ class AddresSerializer(serializers.ModelSerializer):
 
 
 
-class ContactPersonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ("id","email","username",)
 
 """ Company register Serializer """
 class CompanySerializer(WritableNestedModelSerializer,serializers.ModelSerializer):
     user = UserSerializer(many = False)
     addres = AddresSerializer()
-    contact_person = ContactPersonSerializer()
-
     class Meta:
         model = Company
         fields = ("id",'user','name', 'gstin',"contact_person","status","published_date", "addres", 'end_at',)
